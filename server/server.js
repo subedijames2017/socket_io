b817cdcb3036f89f3da03fa0b39d3b34771adb41
Laxman subedi <laxmansubedi10027@gmail.com>
2018-07-09 20:53:59 +0545

    heroku push

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 
-const app = express();
-const port = process.env.PORT||3000;
-app.use(express.static(path.join(__dirname,'../public')));
-app.get('/home',(req,res)=>{
-  res.render('index')
-})
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
 
-//listning to the port
-app.listen(port,()=>{
-  console.log(`server started on port ${port}`);
-})
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
